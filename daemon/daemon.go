4eb3561b61944985faf12fb82a170889a3af263a
Ian Vernon <ian@cilium.io>
2019-05-28 16:57:36 -0700

    daemon: move datapath-related code to separate file
    This reduces the size of `daemon.go`.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -15,31 +15,20 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"net"
 	"os"
-	"path/filepath"
 	"runtime"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/cilium/cilium/api/v1/models"
 	health "github.com/cilium/cilium/cilium-health/launch"
-	"github.com/cilium/cilium/common"
-	"github.com/cilium/cilium/pkg/bpf"
-	"github.com/cilium/cilium/pkg/cgroups"
 	"github.com/cilium/cilium/pkg/clustermesh"
-	"github.com/cilium/cilium/pkg/command/exec"
 	"github.com/cilium/cilium/pkg/completion"
-	"github.com/cilium/cilium/pkg/controller"
 	"github.com/cilium/cilium/pkg/counter"
 	"github.com/cilium/cilium/pkg/datapath"
-	"github.com/cilium/cilium/pkg/datapath/alignchecker"
-	bpfIPCache "github.com/cilium/cilium/pkg/datapath/ipcache"
-	"github.com/cilium/cilium/pkg/datapath/iptables"
 	"github.com/cilium/cilium/pkg/datapath/linux/ipsec"
 	"github.com/cilium/cilium/pkg/datapath/loader"
 	"github.com/cilium/cilium/pkg/datapath/prefilter"
@@ -48,7 +37,6 @@ import (
 	"github.com/cilium/cilium/pkg/endpoint"
 	"github.com/cilium/cilium/pkg/endpoint/connector"
 	"github.com/cilium/cilium/pkg/endpointmanager"
-	"github.com/cilium/cilium/pkg/envoy"
 	"github.com/cilium/cilium/pkg/fqdn"
 	"github.com/cilium/cilium/pkg/identity"
 	"github.com/cilium/cilium/pkg/identity/cache"
@@ -63,12 +51,8 @@ import (
 	"github.com/cilium/cilium/pkg/maps/ctmap"
 	"github.com/cilium/cilium/pkg/maps/eppolicymap"
 	ipcachemap "github.com/cilium/cilium/pkg/maps/ipcache"
-	"github.com/cilium/cilium/pkg/maps/lbmap"
-	"github.com/cilium/cilium/pkg/maps/lxcmap"
-	"github.com/cilium/cilium/pkg/maps/metricsmap"
 	"github.com/cilium/cilium/pkg/maps/policymap"
 	"github.com/cilium/cilium/pkg/maps/sockmap"
-	"github.com/cilium/cilium/pkg/maps/tunnel"
 	monitoragent "github.com/cilium/cilium/pkg/monitor/agent"
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
 	"github.com/cilium/cilium/pkg/mtu"
@@ -319,46 +303,10 @@ func (d *Daemon) DebugEnabled() bool {
 	return option.Config.Opts.IsEnabled(option.Debug)
 }
 
-func (d *Daemon) writeNetdevHeader(dir string) error {
-	headerPath := filepath.Join(dir, common.NetdevHeaderFileName)
-	log.WithField(logfields.Path, headerPath).Debug("writing configuration")
-
-	f, err := os.Create(headerPath)
-	if err != nil {
-		return fmt.Errorf("failed to open file %s for writing: %s", headerPath, err)
-
-	}
-	defer f.Close()
-
-	if err := d.datapath.WriteNetdevConfig(f, d); err != nil {
-		return err
-	}
-	return nil
-}
-
 // GetCIDRPrefixLengths returns the sorted list of unique prefix lengths used
 // by CIDR policies.
 func (d *Daemon) GetCIDRPrefixLengths() (s6, s4 []int) {
 	return d.prefixLengths.ToBPFData()
-}
-
-// Must be called with option.Config.EnablePolicyMU locked.
-func (d *Daemon) writePreFilterHeader(dir string) error {
-	headerPath := filepath.Join(dir, common.PreFilterHeaderFileName)
-	log.WithField(logfields.Path, headerPath).Debug("writing configuration")
-	f, err := os.Create(headerPath)
-	if err != nil {
-		return fmt.Errorf("failed to open file %s for writing: %s", headerPath, err)
-
-	}
-	defer f.Close()
-	fw := bufio.NewWriter(f)
-	fmt.Fprint(fw, "/*\n")
-	fmt.Fprintf(fw, " * XDP device: %s\n", option.Config.DevicePreFilter)
-	fmt.Fprintf(fw, " * XDP mode: %s\n", option.Config.ModePreFilter)
-	fmt.Fprint(fw, " */\n\n")
-	d.preFilter.WriteConfig(fw)
-	return fw.Flush()
 }
 
 // GetOptions returns the datapath configuration options of the daemon.
@@ -415,282 +363,6 @@ func (d *Daemon) GetCompilationLock() *lock.RWMutex {
 	return d.compilationMutex
 }
 
-func (d *Daemon) compileBase() error {
-	var args []string
-	var mode string
-	var ret error
-
-	args = make([]string, initArgMax)
-
-	// Lock so that endpoints cannot be built while we are compile base programs.
-	d.compilationMutex.Lock()
-	defer d.compilationMutex.Unlock()
-
-	if err := d.writeNetdevHeader("./"); err != nil {
-		log.WithError(err).Warn("Unable to write netdev header")
-		return err
-	}
-	loader.Init(d.datapath, &d.nodeDiscovery.LocalConfig)
-
-	scopedLog := log.WithField(logfields.XDPDevice, option.Config.DevicePreFilter)
-	if option.Config.DevicePreFilter != "undefined" {
-		if err := prefilter.ProbePreFilter(option.Config.DevicePreFilter, option.Config.ModePreFilter); err != nil {
-			scopedLog.WithError(err).Warn("Turning off prefilter")
-			option.Config.DevicePreFilter = "undefined"
-		}
-	}
-	if option.Config.DevicePreFilter != "undefined" {
-		if d.preFilter, ret = prefilter.NewPreFilter(); ret != nil {
-			scopedLog.WithError(ret).Warn("Unable to init prefilter")
-			return ret
-		}
-
-		if err := d.writePreFilterHeader("./"); err != nil {
-			scopedLog.WithError(err).Warn("Unable to write prefilter header")
-			return err
-		}
-
-		args[initArgDevicePreFilter] = option.Config.DevicePreFilter
-		args[initArgModePreFilter] = option.Config.ModePreFilter
-	}
-
-	args[initArgLib] = option.Config.BpfDir
-	args[initArgRundir] = option.Config.StateDir
-	args[initArgCgroupRoot] = cgroups.GetCgroupRoot()
-	args[initArgBpffsRoot] = bpf.GetMapRoot()
-
-	if option.Config.EnableIPv4 {
-		args[initArgIPv4NodeIP] = node.GetInternalIPv4().String()
-	} else {
-		args[initArgIPv4NodeIP] = "<nil>"
-	}
-
-	if option.Config.EnableIPv6 {
-		args[initArgIPv6NodeIP] = node.GetIPv6().String()
-	} else {
-		args[initArgIPv6NodeIP] = "<nil>"
-	}
-
-	args[initArgMTU] = fmt.Sprintf("%d", d.mtuConfig.GetDeviceMTU())
-
-	if option.Config.EnableIPSec {
-		args[initArgIPSec] = "true"
-	} else {
-		args[initArgIPSec] = "false"
-	}
-
-	if !option.Config.InstallIptRules && option.Config.Masquerade {
-		args[initArgMasquerade] = "true"
-	} else {
-		args[initArgMasquerade] = "false"
-	}
-
-	if option.Config.EnableHostReachableServices {
-		args[initArgHostReachableServices] = "true"
-	} else {
-		args[initArgHostReachableServices] = "false"
-	}
-
-	if option.Config.EncryptInterface != "" {
-		args[initArgEncryptInterface] = option.Config.EncryptInterface
-	}
-
-	if option.Config.Device != "undefined" {
-		_, err := netlink.LinkByName(option.Config.Device)
-		if err != nil {
-			log.WithError(err).WithField("device", option.Config.Device).Warn("Link does not exist")
-			return err
-		}
-
-		if option.Config.IsLBEnabled() {
-			if option.Config.Device != option.Config.LBInterface {
-				//FIXME: allow different interfaces
-				return fmt.Errorf("Unable to have an interface for LB mode different than snooping interface")
-			}
-			if err := d.setHostAddresses(); err != nil {
-				return err
-			}
-			mode = "lb"
-		} else {
-			if option.Config.DatapathMode == option.DatapathModeIpvlan {
-				mode = "ipvlan"
-			} else {
-				mode = "direct"
-			}
-		}
-
-		args[initArgMode] = mode
-		args[initArgDevice] = option.Config.Device
-
-		args = append(args, option.Config.Device)
-	} else {
-		if option.Config.IsLBEnabled() && strings.ToLower(option.Config.Tunnel) != "disabled" {
-			//FIXME: allow LBMode in tunnel
-			return fmt.Errorf("Unable to run LB mode with tunnel mode")
-		}
-
-		args[initArgMode] = option.Config.Tunnel
-
-		if option.Config.IsFlannelMasterDeviceSet() {
-			args[initArgMode] = "flannel"
-			args[initArgDevice] = option.Config.FlannelMasterDevice
-		}
-	}
-
-	prog := filepath.Join(option.Config.BpfDir, "init.sh")
-	ctx, cancel := context.WithTimeout(context.Background(), defaults.ExecTimeout)
-	defer cancel()
-	cmd := exec.CommandContext(ctx, prog, args...)
-	cmd.Env = bpf.Environment()
-	if _, err := cmd.CombinedOutput(log, true); err != nil {
-		return err
-	}
-
-	if canDisableDwarfRelocations {
-		// Validate alignments of C and Go equivalent structs
-		if err := alignchecker.CheckStructAlignments(defaults.AlignCheckerName); err != nil {
-			log.WithError(err).Fatal("C and Go structs alignment check failed")
-		}
-	} else {
-		log.Warning("Cannot check matching of C and Go common struct alignments due to old LLVM/clang version")
-	}
-
-	if !option.Config.IsFlannelMasterDeviceSet() {
-		d.ipam.ReserveLocalRoutes()
-	}
-
-	if err := d.datapath.Node().NodeConfigurationChanged(d.nodeDiscovery.LocalConfig); err != nil {
-		return err
-	}
-
-	iptablesManager := iptables.IptablesManager{}
-	iptablesManager.Init()
-	// Always remove masquerade rule and then re-add it if required
-	iptablesManager.RemoveRules()
-	if option.Config.InstallIptRules {
-		if err := iptablesManager.InstallRules(option.Config.HostDevice); err != nil {
-			return err
-		}
-	}
-	// Reinstall proxy rules for any running proxies
-	if d.l7Proxy != nil {
-		d.l7Proxy.ReinstallRules()
-	}
-
-	log.Info("Setting sysctl net.core.bpf_jit_enable=1")
-	log.Info("Setting sysctl net.ipv4.conf.all.rp_filter=0")
-	log.Info("Setting sysctl net.ipv6.conf.all.disable_ipv6=0")
-
-	return nil
-}
-
-// initMaps opens all BPF maps (and creates them if they do not exist). This
-// must be done *before* any operations which read BPF maps, especially
-// restoring endpoints and services.
-func (d *Daemon) initMaps() error {
-	if option.Config.DryMode {
-		return nil
-	}
-
-	if _, err := lxcmap.LXCMap.OpenOrCreate(); err != nil {
-		return err
-	}
-
-	// The ipcache is shared between endpoints. Parallel mode needs to be
-	// used to allow existing endpoints that have not been regenerated yet
-	// to continue using the existing ipcache until the endpoint is
-	// regenerated for the first time. Existing endpoints are using a
-	// policy map which is potentially out of sync as local identities are
-	// re-allocated on startup. Parallel mode allows to continue using the
-	// old version until regeneration. Note that the old version is not
-	// updated with new identities. This is fine as any new identity
-	// appearing would require a regeneration of the endpoint anyway in
-	// order for the endpoint to gain the privilege of communication.
-	if _, err := ipcachemap.IPCache.OpenParallel(); err != nil {
-		return err
-	}
-
-	if _, err := metricsmap.Metrics.OpenOrCreate(); err != nil {
-		return err
-	}
-
-	if _, err := tunnel.TunnelMap.OpenOrCreate(); err != nil {
-		return err
-	}
-
-	if err := openServiceMaps(); err != nil {
-		log.WithError(err).Fatal("Unable to open service maps")
-	}
-
-	// Set up the list of IPCache listeners in the daemon, to be
-	// used by syncLXCMap().
-	ipcache.IPIdentityCache.SetListeners([]ipcache.IPIdentityMappingListener{
-		&envoy.NetworkPolicyHostsCache,
-		bpfIPCache.NewListener(d),
-	})
-
-	// Insert local host entries to bpf maps
-	if err := d.syncLXCMap(); err != nil {
-		return err
-	}
-
-	// Start the controller for periodic sync
-	// The purpose of the controller is to ensure that the host entries are
-	// reinserted to the bpf maps if they are ever removed from them.
-	// TODO: Determine if we can get rid of this when we have more rigorous
-	//       desired/realized state implementation for the bpf maps.
-	controller.NewManager().UpdateController("lxcmap-bpf-host-sync",
-		controller.ControllerParams{
-			DoFunc: func(ctx context.Context) error {
-				return d.syncLXCMap()
-			},
-			RunInterval: 5 * time.Second,
-		})
-
-	// Start the controller for periodic sync of the metrics map with
-	// the prometheus server.
-	controller.NewManager().UpdateController("metricsmap-bpf-prom-sync",
-		controller.ControllerParams{
-			DoFunc:      metricsmap.SyncMetricsMap,
-			RunInterval: 5 * time.Second,
-		})
-
-	// Clean all lb entries
-	if !option.Config.RestoreState {
-		log.Debug("cleaning up all BPF LB maps")
-
-		d.loadBalancer.BPFMapMU.Lock()
-		defer d.loadBalancer.BPFMapMU.Unlock()
-
-		if option.Config.EnableIPv6 {
-			if err := lbmap.Service6Map.DeleteAll(); err != nil {
-				return err
-			}
-			if err := lbmap.RRSeq6Map.DeleteAll(); err != nil {
-				return err
-			}
-		}
-		if err := d.RevNATDeleteAll(); err != nil {
-			return err
-		}
-
-		if option.Config.EnableIPv4 {
-			if err := lbmap.Service4Map.DeleteAll(); err != nil {
-				return err
-			}
-			if err := lbmap.RRSeq4Map.DeleteAll(); err != nil {
-				return err
-			}
-		}
-
-		// If we are not restoring state, all endpoints can be
-		// deleted. Entries will be re-populated.
-		lxcmap.LXCMap.DeleteAll()
-	}
-
-	return nil
-}
-
 func (d *Daemon) init() error {
 	globalsDir := option.Config.GetGlobalsDir()
 	if err := os.MkdirAll(globalsDir, defaults.RuntimePathRights); err != nil {
@@ -733,127 +405,6 @@ func (d *Daemon) init() error {
 	return nil
 }
 
-func (d *Daemon) createNodeConfigHeaderfile() error {
-	nodeConfigPath := option.Config.GetNodeConfigPath()
-	f, err := os.Create(nodeConfigPath)
-	if err != nil {
-		log.WithError(err).WithField(logfields.Path, nodeConfigPath).Fatal("Failed to create node configuration file")
-		return err
-	}
-	defer f.Close()
-
-	if err = d.datapath.WriteNodeConfig(f, &d.nodeDiscovery.LocalConfig); err != nil {
-		log.WithError(err).WithField(logfields.Path, nodeConfigPath).Fatal("Failed to write node configuration file")
-		return err
-	}
-	return nil
-}
-
-// syncLXCMap adds local host enties to bpf lxcmap, as well as
-// ipcache, if needed, and also notifies the daemon and network policy
-// hosts cache if changes were made.
-func (d *Daemon) syncLXCMap() error {
-	// TODO: Update addresses first, in case node addressing has changed.
-	// TODO: Once these start changing on runtime, figure out the locking strategy.
-	specialIdentities := []identity.IPIdentityPair{}
-
-	if option.Config.EnableIPv4 {
-		ip := node.GetInternalIPv4()
-		if len(ip) > 0 {
-			specialIdentities = append(specialIdentities,
-				identity.IPIdentityPair{
-					IP: ip,
-					ID: identity.ReservedIdentityHost,
-				})
-		}
-
-		ip = node.GetExternalIPv4()
-		if len(ip) > 0 {
-			specialIdentities = append(specialIdentities,
-				identity.IPIdentityPair{
-					IP: ip,
-					ID: identity.ReservedIdentityHost,
-				})
-		}
-
-		specialIdentities = append(specialIdentities,
-			identity.IPIdentityPair{
-				IP:   net.IPv4zero,
-				Mask: net.CIDRMask(0, net.IPv4len*8),
-				ID:   identity.ReservedIdentityWorld,
-			})
-	}
-
-	if option.Config.EnableIPv6 {
-		ip := node.GetIPv6()
-		if len(ip) > 0 {
-			specialIdentities = append(specialIdentities,
-				identity.IPIdentityPair{
-					IP: ip,
-					ID: identity.ReservedIdentityHost,
-				})
-		}
-
-		ip = node.GetIPv6Router()
-		if len(ip) > 0 {
-			specialIdentities = append(specialIdentities,
-				identity.IPIdentityPair{
-					IP: ip,
-					ID: identity.ReservedIdentityHost,
-				})
-		}
-
-		specialIdentities = append(specialIdentities,
-			identity.IPIdentityPair{
-				IP:   net.IPv6zero,
-				Mask: net.CIDRMask(0, net.IPv6len*8),
-				ID:   identity.ReservedIdentityWorld,
-			})
-	}
-
-	existingEndpoints, err := lxcmap.DumpToMap()
-	if err != nil {
-		return err
-	}
-
-	for _, ipIDPair := range specialIdentities {
-		hostKey := node.GetIPsecKeyIdentity()
-		isHost := ipIDPair.ID == identity.ReservedIdentityHost
-		if isHost {
-			added, err := lxcmap.SyncHostEntry(ipIDPair.IP)
-			if err != nil {
-				return fmt.Errorf("Unable to add host entry to endpoint map: %s", err)
-			}
-			if added {
-				log.WithField(logfields.IPAddr, ipIDPair.IP).Debugf("Added local ip to endpoint map")
-			}
-		}
-
-		delete(existingEndpoints, ipIDPair.IP.String())
-
-		// Upsert will not propagate (reserved:foo->ID) mappings across the cluster,
-		// and we specifically don't want to do so.
-		ipcache.IPIdentityCache.Upsert(ipIDPair.PrefixString(), nil, hostKey, ipcache.Identity{
-			ID:     ipIDPair.ID,
-			Source: ipcache.FromAgentLocal,
-		})
-	}
-
-	for hostIP, info := range existingEndpoints {
-		if ip := net.ParseIP(hostIP); info.IsHost() && ip != nil {
-			if err := lxcmap.DeleteEntry(ip); err != nil {
-				log.WithError(err).WithFields(logrus.Fields{
-					logfields.IPAddr: hostIP,
-				}).Warn("Unable to delete obsolete host IP from BPF map")
-			} else {
-				log.Debugf("Removed outdated host ip %s from endpoint map", hostIP)
-			}
-		}
-	}
-
-	return nil
-}
-
 func createIPNet(ones, bits int) *net.IPNet {
 	return &net.IPNet{
 		Mask: net.CIDRMask(ones, bits),
@@ -883,18 +434,6 @@ func createPrefixLengthCounter() *counter.PrefixLengthCounter {
 		log.WithError(err).Fatal("Failed to create default prefix lengths")
 	}
 	return counter
-}
-
-func deleteHostDevice() {
-	link, err := netlink.LinkByName(option.Config.HostDevice)
-	if err != nil {
-		log.WithError(err).Warningf("Unable to lookup host device %s. No old cilium_host interface exists", option.Config.HostDevice)
-		return
-	}
-
-	if err := netlink.LinkDel(link); err != nil {
-		log.WithError(err).Errorf("Unable to delete host device %s to change allocation CIDR", option.Config.HostDevice)
-	}
 }
 
 // NewDaemon creates and returns a new Daemon with the parameters set in c.
@@ -1256,53 +795,6 @@ func changedOption(key string, value option.OptionSetting, data interface{}) {
 		proxy.ChangeLogLevel(logging.GetLevel(logging.DefaultLogger))
 	}
 	d.policy.BumpRevision() // force policy recalculation
-}
-
-// listFilterIfs returns a map of interfaces based on the given filter.
-// The filter should take a link and, if found, return the index of that
-// interface, if not found return -1.
-func listFilterIfs(filter func(netlink.Link) int) (map[int]netlink.Link, error) {
-	ifs, err := netlink.LinkList()
-	if err != nil {
-		return nil, err
-	}
-	vethLXCIdxs := map[int]netlink.Link{}
-	for _, intf := range ifs {
-		if idx := filter(intf); idx != -1 {
-			vethLXCIdxs[idx] = intf
-		}
-	}
-	return vethLXCIdxs, nil
-}
-
-// clearCiliumVeths checks all veths created by cilium and removes all that
-// are considered a leftover from failed attempts to connect the container.
-func (d *Daemon) clearCiliumVeths() error {
-	log.Info("Removing stale endpoint interfaces")
-
-	leftVeths, err := listFilterIfs(func(intf netlink.Link) int {
-		// Filter by veth and return the index of the interface.
-		if intf.Type() == "veth" {
-			return intf.Attrs().Index
-		}
-		return -1
-	})
-
-	if err != nil {
-		return fmt.Errorf("unable to retrieve host network interfaces: %s", err)
-	}
-
-	for _, v := range leftVeths {
-		peerIndex := v.Attrs().ParentIndex
-		parentVeth, found := leftVeths[peerIndex]
-		if found && peerIndex != 0 && strings.HasPrefix(parentVeth.Attrs().Name, "lxc") {
-			err := netlink.LinkDel(v)
-			if err != nil {
-				log.WithError(err).Warningf("Unable to delete stale veth device %s", v.Attrs().Name)
-			}
-		}
-	}
-	return nil
 }
 
 // numWorkerThreads returns the number of worker threads with a minimum of 4.
